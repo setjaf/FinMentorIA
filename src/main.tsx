232ea600c4e36ac7b771524bbdb5090e06665d71
setjaf <setjafet@gmail.com>
2025-08-11 01:20:57 -0600

    Quitar basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/FinMentorIA/'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
